6ee1907745bcbb9f70165603406a3cb6e236f0ea
Matt Glover <matthewglover@me.com>
2016-10-11 15:17:10 +0100

    update api uri for production setting

--- app/api/helpers.js
+++ app/api/helpers.js
@@ -1,8 +1,13 @@
 // @flow
 import config from '../config.json';
 
+const apiUrl =
+  process.env.NODE_ENV === 'production'
+    ? config.productionUrl
+    : config.testUrl;
+
 export const loginRoute =
-  `${config.apiUrl}${config.authPath}`;
+  `${apiUrl}${config.authPath}`;
 
 export const verifyTokenRoute = (jwt: string) =>
-  `${config.apiUrl}${config.verifyTokenPath}?jwt=${jwt}`;
+  `${apiUrl}${config.verifyTokenPath}?jwt=${jwt}`;
