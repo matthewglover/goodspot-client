d06b6d8b977e0376ccc98172d531d5ff7e8c66e6
Matt Glover <matthewglover@me.com>
2016-10-13 13:22:45 +0100

    update tests and coverage settings

--- test/reducers/locality_search_reducer.test.js
+++ test/reducers/locality_search_reducer.test.js
@@ -1,15 +1,17 @@
 /* eslint-disable max-len */
 import test from 'ava';
-import localitySearch from '../../app/reducers/locality_search_reducer';
+import localitySearch, * as fromLocalitySearch from '../../app/reducers/locality_search_reducer';
 import {
   LOCALITY_SEARCH_RESULTS } from '../../app/action_types';
 
+const localities = [
+  { description: 'Bethnal Green', id: '12345' },
+  { description: 'Honor Oak Park', id: '4324' },
+];
+
 const localitySearchAction = {
   type: LOCALITY_SEARCH_RESULTS,
-  localities: [
-    { description: 'Bethnal Green', id: '12345' },
-    { description: 'Honor Oak Park', id: '4324' },
-  ],
+  localities,
 };
 
 
@@ -20,5 +22,10 @@ test('localitySearchReducer default state is an empty array', (t) => {
 
 test('localitySearchReducer updates localites with results from action', (t) => {
   const actual = localitySearch(undefined, localitySearchAction);
-  t.is(actual, localitySearchAction.localities);
+  t.is(actual, localities);
+});
+
+test('getLocalitySearchResults returns localities', (t) => {
+  const actual = fromLocalitySearch.getLocalitySearchResults(localities);
+  t.is(actual, localities);
 });
